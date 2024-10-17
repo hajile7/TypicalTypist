c541c3c1c6a9c214d108ce1841ae1465d818fdb9
Eli <elireid.28@gmail.com>
2024-10-17 14:44:42 -0400

    first; setup words service + quickly listed out a test instancefor testing purposes

---
+++ Models/WordTestObject.cs
@@ -0,0 +1,8 @@
+namespace TypicalTypist.Models
+{
+    public class WordTestObject
+    {
+        public List<string> Chars { get; set; } = [];
+        public int StartIndex { get; set;} = 0;
+    }
+}
---
+++ Services/WordService.cs
@@ -0,0 +1,21 @@
+using System.Net.Http.Json;
+using TypicalTypist.Models;
+
+namespace TypicalTypist.Services
+{
+    public class WordService(HttpClient httpClient)
+    {
+        private readonly HttpClient _httpClient = httpClient;
+        private readonly string url = "https://localhost:7192/";
+
+        public async Task<List<WordTestObject>> GetRandomTest() {
+            List<WordTestObject> result = await _httpClient.GetFromJsonAsync<List<WordTestObject>>($"{url}api/Words/Random") 
+            ?? throw new InvalidOperationException("The response from the API was null.");
+            return result;
+        }
+
+    }
+
+
+
+}
