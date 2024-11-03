a0e90a176c4cb727a3a44627a695dfb56f2fb07d
Eli <elireid.28@gmail.com>
2024-11-03 11:31:29 -0500

    added symbols to legal chars in test file and confirmed they're functioning

--- obj/Debug/net8.0/TypicalTypist.AssemblyInfo.cs
+++ obj/Debug/net8.0/TypicalTypist.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TypicalTypist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cce2c4499b80f1eb0edccb2d148a944c0e036a2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c96f5ac41dc785e9f52483313d1008be484fa91")]
 [assembly: System.Reflection.AssemblyProductAttribute("TypicalTypist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TypicalTypist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
