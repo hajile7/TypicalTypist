782f197769d53b2947d58ac7b2fa3a06c440ff7c
Eli <elireid.28@gmail.com>
2024-11-11 13:55:36 -0500

    All stats appear to be working as expected in test file. Plan to do a bit more testing to confirm before adding changes to main file.

--- obj/Debug/net8.0/TypicalTypist.AssemblyInfo.cs
+++ obj/Debug/net8.0/TypicalTypist.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TypicalTypist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe98d5a58ae6ee7fb37e342ed0268ea9f891cd4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39dcfc5ff0409a8cd6faf47f7d29ee310c75a9f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TypicalTypist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TypicalTypist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
