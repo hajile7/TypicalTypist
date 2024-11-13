57810573c5762fdca57feb92bcbfef46b4b97fb4
Eli <elireid.28@gmail.com>
2024-11-13 10:37:36 -0500

    Now able to change test size. Modes are next.

--- Services/WordService.cs
+++ Services/WordService.cs
@@ -8,20 +8,20 @@ namespace TypicalTypist.Services
         private readonly HttpClient httpClient = _httpClient;
         private readonly string url = "https://localhost:7258";
 
-        public async Task<List<WordTestObject>> GetRandomWordsTest() {
-            List<WordTestObject> result = await httpClient.GetFromJsonAsync<List<WordTestObject>>($"{url}/api/Words/Random") 
+        public async Task<List<WordTestObject>> GetRandomWordsTest(int x) {
+            List<WordTestObject> result = await httpClient.GetFromJsonAsync<List<WordTestObject>>($"{url}/api/Words/Random/{x}") 
             ?? throw new InvalidOperationException("The response from the API was null.");
             return result;
         }
 
-        public async Task<List<WordTestObject>> GetRandomCapsTest() {
-            List<WordTestObject> result = await httpClient.GetFromJsonAsync<List<WordTestObject>>($"{url}/api/Words/RandomCaps") 
+        public async Task<List<WordTestObject>> GetRandomCapsTest(int y) {
+            List<WordTestObject> result = await httpClient.GetFromJsonAsync<List<WordTestObject>>($"{url}/api/Words/RandomCaps/{y}") 
             ?? throw new InvalidOperationException("The response from the API was null.");
             return result;
         }
 
-        public async Task<List<WordTestObject>> GetRandomWordsAndNumbersTest() {
-            List<WordTestObject> result = await httpClient.GetFromJsonAsync<List<WordTestObject>>($"{url}/api/Words/RandomNumbers") 
+        public async Task<List<WordTestObject>> GetRandomWordsAndNumbersTest(int z) {
+            List<WordTestObject> result = await httpClient.GetFromJsonAsync<List<WordTestObject>>($"{url}/api/Words/RandomNumbers/{z}") 
             ?? throw new InvalidOperationException("The response from the API was null.");
             return result;
         }
--- obj/Debug/net8.0/TypicalTypist.AssemblyInfo.cs
+++ obj/Debug/net8.0/TypicalTypist.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TypicalTypist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc5f835a406ea55338df1a257e997e9ab5401a69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2588b4a543b300db12b9e554a71ed4480afdf2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TypicalTypist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TypicalTypist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
