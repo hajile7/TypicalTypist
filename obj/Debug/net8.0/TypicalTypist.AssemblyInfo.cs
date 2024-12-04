0f3e58662bb8f0fa707bbf7d6b2afef8f6f3c00d
Eli <elireid.28@gmail.com>
2024-12-04 10:05:56 -0500

    finished up previous test section in user profile

--- obj/Debug/net8.0/TypicalTypist.AssemblyInfo.cs
+++ obj/Debug/net8.0/TypicalTypist.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TypicalTypist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdda7c3b6e72241944a0e6759af99742f3687e21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3526eb4dc2853e404ee6fbe6a3652014b3799b7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TypicalTypist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TypicalTypist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
