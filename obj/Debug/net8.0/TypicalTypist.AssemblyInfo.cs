8c937fc417e2d0972bf09633ab513827bad3402e
Eli <elireid.28@gmail.com>
2024-12-27 10:24:51 -0500

    Added meta property for title and site name

--- obj/Debug/net8.0/TypicalTypist.AssemblyInfo.cs
+++ obj/Debug/net8.0/TypicalTypist.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TypicalTypist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3da1e253fb91470af2b7b76beece95e3cd095e9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbbc29abf0f5338938925d404e37122886176664")]
 [assembly: System.Reflection.AssemblyProductAttribute("TypicalTypist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TypicalTypist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
